88e0d79876af6a25ff28646644eaa1b866330ce8
David Ferrand <david@composed.com>
2016-06-08 23:20:35 +0100

    Define main colours for electricity and gas

--- app/src/main/java/com/davidferrand/spark/data/FuelType.java
+++ app/src/main/java/com/davidferrand/spark/data/FuelType.java
@@ -3,17 +3,29 @@ package com.davidferrand.spark.data;
 import com.davidferrand.spark.R;
 
 public enum FuelType {
-    ELECTRICITY(R.drawable.ic_power_white_24dp, R.string.electricity),
-    GAS(R.drawable.ic_whatshot_white_24dp, R.string.gas);
+    ELECTRICITY(
+            R.drawable.ic_power_white_24dp,
+            R.string.electricity,
+            R.color.electricity_colorPrimary,
+            R.color.electricity_colorPrimaryDark),
+    GAS(
+            R.drawable.ic_whatshot_white_24dp,
+            R.string.gas,
+            R.color.gas_colorPrimary,
+            R.color.gas_colorPrimaryDark);
 
     public static final FuelType[] VALUES = FuelType.values();
 
     public final int iconRes;
     public final int nameRes;
+    public final int colorPrimaryRes;
+    public final int colorPrimaryDarkRes;
 
-    FuelType(int iconTabRes, int nameRes) {
+    FuelType(int iconTabRes, int nameRes, int colorPrimaryRes, int colorPrimaryDarkRes) {
         this.iconRes = iconTabRes;
         this.nameRes = nameRes;
+        this.colorPrimaryRes = colorPrimaryRes;
+        this.colorPrimaryDarkRes = colorPrimaryDarkRes;
     }
 
     public static FuelType valueOf(int position) {
